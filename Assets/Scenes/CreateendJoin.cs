de8da5b40f5aa3fead68312b632b4c72825ac052
Kubmus <159161008+Piotrek222222@users.noreply.github.com>
2025-04-08 16:51:27 +0200

    photon

---
+++ Assets/Scenes/CreateendJoin.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using TMPro;
+
+public class CreateendJoin : MonoBehaviourPunCallbacks
+{
+  public TMP_InputField input_Create;
+  public TMP_InputField input_Join;
+
+  public void  CreateRoom()
+  {
+    PhotonNetwork.CreateRoom(input_Create.text);
+
+  }
+
+
+  public void  JoinRoom()
+  {
+    PhotonNetwork.JoinRoom(input_Join.text);
+
+  }
+
+  public override void OnJoinedRoom()
+  {
+    PhotonNetwork.LoadLevel("LevelTest");
+  }
+}
